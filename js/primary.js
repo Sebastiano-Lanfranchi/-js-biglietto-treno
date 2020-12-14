d820eede3c0efa80572865325549b1b1aad11ebe
Sebastiano-Lanfranchi <sebastiano.lanfranchi@gmail.com>
2020-12-14 15:48:53 +0100

    esercizio finito + verifica

--- js/primary.js
+++ js/primary.js
@@ -1,11 +1,15 @@
-var kilometri = prompt('Inserisci il numero di kilometri da percorrere')
-var anniUtente = prompt('Inserisci la tua etá')
-var prezzo = kilometri * 0.21
+var kilometri = prompt('Inserisci il numero di kilometri da percorrere');
+var anniUtente = prompt('Inserisci la tua etá');
+var prezzo = kilometri * 0.21;
 
-if (anniUtente<18) {
-  prezzo = prezzo * 0.8
-}
-if (anniUtente>65)
-prezzo = prezzo * 0.6
-
-document.getElementById("prezzo").innerText = 'Il prezzo del biglietto é di' + prezzo + '€';
+if (isNaN(kilometri + anniUtente)){
+		alert('Il valore inserito non è numerico');
+	} else {
+    if (anniUtente<18) {
+      prezzo = prezzo * 0.8
+    };
+    if (anniUtente>65){
+      prezzo = prezzo * 0.6
+    };
+	}
+document.getElementById("prezzo").innerText = 'Il prezzo del biglietto é di ' + prezzo + '€';
